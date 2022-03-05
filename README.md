23f5f7546fc3ec028d95692023c6742a8dea24b6
James Tufarelli <jspamless@gmail.com>
2022-03-05 14:41:49 -0800

    Testing adding params

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # save-image
 Saves an image from a url into your repo
+
+Example image:
+
+![wakatime](./media/wakatime.svg)
+![test](./media/test.png)
--- dist/index.js
+++ dist/index.js
@@ -11527,21 +11527,40 @@ var __webpack_exports__ = {};
 (() => {
 "use strict";
 
-const core = __nccwpck_require__(115);
 const github = __nccwpck_require__(3007);
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-}
-catch (error) {
-    core.setFailed(error.message);
-}
+const core = __nccwpck_require__(115);
+const fs = __nccwpck_require__(7147);
+const fetch = __nccwpck_require__(2133);
+const main = async () => {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const url = core.getInput('url');
+        const imagePath = core.getInput('imagePath');
+        const deleteOnFail = core.getInput('deleteOnFail');
+        // const url = 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80'
+        // const url = 'https://minituff-github-readme-stats.vercel.app/api/wakatime?username=minituff&langs_count=18&layout=compact&custom_title=My%20Code%20Stats'
+        // const imagePath = '../media/test.svg'
+        console.log(`url ${url}`);
+        console.log(`imagePath ${imagePath}`);
+        console.log(`deleteOnFail ${deleteOnFail}`);
+        // console.log(path.resolve(imagePath))
+        async function downloadImage() {
+            const response = await fetch(url);
+            const buffer = await response.buffer();
+            fs.writeFile(imagePath, buffer, () => console.log('finished downloading!'));
+        }
+        await downloadImage();
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        console.log(`The event payload: ${payload}`);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+};
+main();
 //# sourceMappingURL=index.js.map
 })();
 
